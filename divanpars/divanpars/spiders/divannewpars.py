7e1a61155e1b17b9bff59348ea2c49a4dbf4ea88
DeniCoder <denis2042@yandex.ru>
2025-03-23 21:04:00 +0300

    change code
    Модификация кода для парсинга первой страницы с сайта диван.ру каталог освещение, выводит данные о названии, цене и дает ссылку на индивидуальную страницу продукта

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -3,8 +3,16 @@ import scrapy
 
 class DivannewparsSpider(scrapy.Spider):
     name = "divannewpars"
-    allowed_domains = ["https://www.divan.ru"]
+    allowed_domains = ["www.divan.ru"]
     start_urls = ["https://www.divan.ru/category/svet"]
 
     def parse(self, response):
-        pass
+        lights = response.css('div._Ud0k')
+        for light in lights:
+            relative_url = light.css('a').attrib['href']
+            full_url = response.urljoin(relative_url)
+            yield {
+                'name' : light.css('div.lsooF span::text').get(),
+                'price' : light.css('div.pY3d2 span::text').get(),
+                'url' : full_url,
+            }
